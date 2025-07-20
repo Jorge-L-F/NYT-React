7e07ca2f18d60cda2a0de6e349beff197bf2a4c8
Jorge-L-F <jorge.l.ferreira99@protonmail.com>
2025-07-20 16:48:46 +0100

    Corrected the condition for the solo news details and fixed the NewsObject component code.

--- src/app/news-object.js
+++ src/app/news-object.js
@@ -6,31 +6,29 @@ function Multimedia({multimedia}) {
 }
 
 function NewsDetails({news}) {
-    if (news.multimedia != null && news.multimedia != undefined && news.multimedia.length) {
-      return 
-        <div className="solo-news-details">
-          <p className="news-title"><b>{news.title}</b></p>
-          <p className="news-text news-abstract">{news.abstract}</p>
-          <p className="news-text">{news.byline}</p>
-          <p className="news-text"><a href={news.url}>Source</a></p>
-        </div>
-    }
-    else {
-      return
-        <div className="news-details">
-          <p className="news-title"><b>{news.title}</b></p>
-          <p className="news-text news-abstract">{news.abstract}</p>
-          <p className="news-text">{news.byline}</p>
-          <p className="news-text"><a href={news.url}>Source</a></p>
-        </div>
-    }
+  if (news.multimedia == null || news.multimedia == undefined || !news.multimedia.length) {
+    return <div className="solo-news-details">
+            <p className="news-title"><b>{news.title}</b></p>
+            <p className="news-text news-abstract">{news.abstract}</p>
+            <p className="news-text">{news.byline}</p>
+            <p className="news-text"><a href={news.url}>Source</a></p>
+          </div>
+  }
+  else {
+    return <div className="news-details">
+            <p className="news-title"><b>{news.title}</b></p>
+            <p className="news-text news-abstract">{news.abstract}</p>
+            <p className="news-text">{news.byline}</p>
+            <p className="news-text"><a href={news.url}>Source</a></p>
+          </div>
+  }
 }
 
-export default function NewsObject(news) {
+export default function NewsObject(info) {
   return (
     <div className="news-object">
-      <Multimedia multimedia={news.multimedia}/>
-      <NewsDetails news={news}/>
+      <Multimedia multimedia={info.news.multimedia}/>
+      <NewsDetails news={info.news}/>
     </div>
   );
 }
