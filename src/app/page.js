96671b73a82867b74aa89183ab2a93df0aa5c0bc
Jorge-L-F <jorge.l.ferreira99@protonmail.com>
2025-07-20 17:02:37 +0100

    Added the conditional elements to the page. Deleted the component with the useless server function. The page was refactored to fetch from the server api.

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,19 @@
 
 //import Image from "next/image";
 //import styles from "./page.module.css";
-import { useState } from "react";
-import { fetchFeed } from "./server";
+import { useState, lazy } from "react";
+
+const NewsObject = lazy(() => import("./news-object"));
+
+function InitialMessage({newspaper, errorResponse}) {
+  if (!newspaper.length && !errorResponse)
+    return <p className="initial-message">Click the fetch button to fetch the latest news.</p>
+}
+
+function ErrorMessage({errorResponse}) {
+  if (errorResponse)
+    return <p className="error-message">{errorResponse}</p>
+}
 
 export default function Home() {
   const [title] = useState('NYT Feed');
@@ -11,23 +22,31 @@ export default function Home() {
   const [errorResponse, setError] = useState(null);
 
   function fetchNews() {
-    const data = fetchFeed();
-    if (data instanceof Array) {
-      setNews(data);
-      setError(null);
-    }
-    else if (data instanceof String) {
-      errorMessage = "There was a problem while fetching the articles from NYT. Try refreshing the page.\n" + data;
-    }
-
-    setError(errorMessage);
+    fetch('/api/news').then(response => response.json()).then(data => {
+      if (data instanceof Array) {
+        setNews(data);
+        setError(null);
+      }
+      else if (data instanceof String) {
+        let errorMessage = "There was a problem while fetching the articles from NYT. Try refreshing the page.\n" + data;
+        setError(errorMessage);
+      }
+    },
+    error => {
+      const errorMessage = "There was a problem while fetching the articles from NYT. Try refreshing the page.\n" + error;
+      setError(errorMessage);
+    })
   };
+
+  const news = newspaper.map((news, index) => <NewsObject key={index} news={news}/>);
 
   return (
     <div className="feed-container">
       <button onClick={fetchNews} className="fetch-button"><b>Fetch News</b></button>
       <div className="scrollable-box">
-        
+        <InitialMessage newspaper={newspaper} errorResponse={errorResponse}/>
+        <ErrorMessage errorResponse={errorResponse}/>
+        {news}
       </div>
     </div>
     /*<div className={styles.page}>
