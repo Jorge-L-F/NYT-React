254e5e940c03c296d1996749992c4b9890fbc071
Jorge-L-F <jorge.l.ferreira99@protonmail.com>
2025-07-20 17:45:11 +0100

    Removed useless configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            new URL('https://developer.nytimes.com/files/poweredby_nytimes_200a.png?v=1583354208344')
-        ],
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
