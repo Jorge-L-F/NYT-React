9cf47853e871c0b2f69d2ce03081e88fa6693d50
Jorge-L-F <jorge.l.ferreira99@protonmail.com>
2025-07-22 23:42:36 +0100

    Set the title on the top bar with the title in the metadata object.

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <body>
         <div className="navigation-bar">
-          <b className="title">NYT Feed</b>
+          <b className="title">{metadata.title}</b>
         </div>
         {children}
         <footer>
