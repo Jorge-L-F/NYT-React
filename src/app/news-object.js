991de601621fb01515bcf0ed1640dcdb4ea9e4b3
Jorge-L-F <jorge.l.ferreira99@protonmail.com>
2025-07-24 23:33:58 +0100

    Added extra logic to the NewsObject component to make it pass the tests.

--- src/app/news-object.js
+++ src/app/news-object.js
@@ -6,21 +6,23 @@ function Multimedia({multimedia}) {
 }
 
 function NewsDetails({news}) {
-  if (news.multimedia == null || news.multimedia == undefined || !news.multimedia.length) {
-    return <div className="solo-news-details">
-            <p className="news-title"><b>{news.title}</b></p>
-            <p className="news-text news-abstract">{news.abstract}</p>
-            <p className="news-text">{news.byline}</p>
-            <p className="news-text"><a href={news.url}>Source</a></p>
-          </div>
-  }
-  else {
-    return <div className="news-details">
-            <p className="news-title"><b>{news.title}</b></p>
-            <p className="news-text news-abstract">{news.abstract}</p>
-            <p className="news-text">{news.byline}</p>
-            <p className="news-text"><a href={news.url}>Source</a></p>
-          </div>
+  if ((news.title || news.abstract) && news.url) {
+    if (news.multimedia == null || news.multimedia == undefined || !news.multimedia.length) {
+      return <div className="solo-news-details">
+              <p className="news-title"><b>{news.title}</b></p>
+              <p className="news-text news-abstract">{news.abstract}</p>
+              <p className="news-text">{news.byline}</p>
+              <p className="news-text"><a href={news.url}>Source</a></p>
+            </div>
+    }
+    else {
+      return <div className="news-details">
+              <p className="news-title"><b>{news.title}</b></p>
+              <p className="news-text news-abstract">{news.abstract}</p>
+              <p className="news-text">{news.byline}</p>
+              <p className="news-text"><a href={news.url}>Source</a></p>
+            </div>
+    }
   }
 }
 
