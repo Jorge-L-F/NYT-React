fcfbe19598ecb9ba308e2fc611193a1ee9a1824e
Jorge-L-F <jorge.l.ferreira99@protonmail.com>
2025-07-24 23:39:08 +0100

    Minor code style change.

--- src/app/news-object.js
+++ src/app/news-object.js
@@ -2,26 +2,30 @@
 
 function Multimedia({multimedia}) {
     if (multimedia != null && multimedia != undefined && multimedia.length)
-      return <img src={multimedia[multimedia.length - 1].url} alt="News Image" className="news-image"/>
+      return (<img src={multimedia[multimedia.length - 1].url} alt="News Image" className="news-image"/>);
 }
 
 function NewsDetails({news}) {
   if ((news.title || news.abstract) && news.url) {
     if (news.multimedia == null || news.multimedia == undefined || !news.multimedia.length) {
-      return <div className="solo-news-details">
-              <p className="news-title"><b>{news.title}</b></p>
-              <p className="news-text news-abstract">{news.abstract}</p>
-              <p className="news-text">{news.byline}</p>
-              <p className="news-text"><a href={news.url}>Source</a></p>
-            </div>
+      return (
+        <div className="solo-news-details">
+          <p className="news-title"><b>{news.title}</b></p>
+          <p className="news-text news-abstract">{news.abstract}</p>
+          <p className="news-text">{news.byline}</p>
+          <p className="news-text"><a href={news.url}>Source</a></p>
+        </div>
+      );
     }
     else {
-      return <div className="news-details">
-              <p className="news-title"><b>{news.title}</b></p>
-              <p className="news-text news-abstract">{news.abstract}</p>
-              <p className="news-text">{news.byline}</p>
-              <p className="news-text"><a href={news.url}>Source</a></p>
-            </div>
+      return (
+        <div className="news-details">
+          <p className="news-title"><b>{news.title}</b></p>
+          <p className="news-text news-abstract">{news.abstract}</p>
+          <p className="news-text">{news.byline}</p>
+          <p className="news-text"><a href={news.url}>Source</a></p>
+        </div>
+      );
     }
   }
 }
