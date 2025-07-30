7ca10bbef4e6450104a13850433cdc2d032766e1
Jorge-L-F <jorge.l.ferreira99@protonmail.com>
2025-07-30 22:34:31 +0100

    Refactored the code of the NewsObject component to make it cleaner.

--- src/app/news-object.js
+++ src/app/news-object.js
@@ -8,45 +8,50 @@ function validateDetails(news) {
   return (news.title || news.abstract) && news.byline && typeof news.byline == "string" && news.byline.trim().length > 3 && news.byline.trim().startsWith("By ") && news.url;
 }
 
-function validateEntry(news) {
-  return validateMultimedia(news.multimedia) && validateDetails(news);
-}
-
-function Multimedia({news}) {
-    if (validateEntry(news))
-      return (<img src={news.multimedia[news.multimedia.length - 1].url} alt="News Image" className="news-image"/>);
+function Multimedia({multimedia}) {
+  return (<img src={multimedia[multimedia.length - 1].url} alt="News Image" className="news-image"/>);
 }
 
 function NewsDetails({news}) {
-  if (validateDetails(news)) {
-    if (!validateMultimedia(news.multimedia)) {
-      return (
-        <div className="solo-news-details">
-          <p className="news-title"><b>{news.title}</b></p>
-          <p className="news-text news-abstract">{news.abstract}</p>
-          <p className="news-text">{news.byline}</p>
-          <p className="news-text"><a href={news.url}>Source</a></p>
-        </div>
-      );
-    }
-    else {
-      return (
-        <div className="news-details">
-          <p className="news-title"><b>{news.title}</b></p>
-          <p className="news-text news-abstract">{news.abstract}</p>
-          <p className="news-text">{news.byline}</p>
-          <p className="news-text"><a href={news.url}>Source</a></p>
-        </div>
-      );
-    }
-  }
+  return (
+    <div className="news-details">
+      <p className="news-title"><b>{news.title}</b></p>
+      <p className="news-text news-abstract">{news.abstract}</p>
+      <p className="news-text">{news.byline}</p>
+      <p className="news-text"><a href={news.url}>Source</a></p>
+    </div>
+  );
+}
+
+function SoloNewsDetails({news}) {
+  return (
+    <div className="solo-news-details">
+      <p className="news-title"><b>{news.title}</b></p>
+      <p className="news-text news-abstract">{news.abstract}</p>
+      <p className="news-text">{news.byline}</p>
+      <p className="news-text"><a href={news.url}>Source</a></p>
+    </div>
+  );
 }
 
 export default function NewsObject(info) {
-  return (
-    <div className="news-object">
-      <Multimedia news={info.news}/>
-      <NewsDetails news={info.news}/>
-    </div>
-  );
+  let news = info.news;
+  let multimedia = news.multimedia;
+  if (validateDetails(news)) {
+    if (validateMultimedia(multimedia))
+      return (
+        <div className="news-object">
+          <Multimedia multimedia={multimedia}/>
+          <NewsDetails news={news}/>
+        </div>
+      );
+    else
+      return (
+        <div className="news-object">
+          <SoloNewsDetails news={news}/>
+        </div>
+      );
+  }
+  else
+    return (<div className="news-object"/>);
 }
